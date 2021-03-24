c3a53fbd66cfdb859c1bfcc07aca5a4efc8ec97a
Juliancio Carvalho <juliancio_alves@hotmail.com>
2021-03-23 22:51:41 -0300

    feat: list categories

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,19 +1,31 @@
-import { Category } from "../model/Category"
+import { Category } from "../model/Category";
 
-class CategoriesRepository {
-    private categories: Category[]
-
-    constructor() {
-        this.categories = []
-    }
-
-    create() {
-        Object.assign(category, {
-            name,
-            description,
-            created_at: new Date(),
-          });
-        
-          categories.push(category);
-    }
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
 }
+class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+}
+
+export { CategoriesRepository };
